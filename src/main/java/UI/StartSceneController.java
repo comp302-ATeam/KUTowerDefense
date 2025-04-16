7a79d881f8c8ecb78121c3b7064ca16d6f0df11b
Cemkayim22 <ckayim22@ku.edu.tr>
2025-04-16 15:43:31 +0300

    main test 01

---
+++ src/main/java/UI/StartSceneController.java
@@ -0,0 +1,42 @@
+package UI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartSceneController {
+
+//    @FXML
+//    private Button buttonStartNew;
+//    @FXML
+//    private Button buttonLoadGame;
+//    @FXML
+//    private Button buttonOptions;
+//    @FXML
+//    private Button buttonQuit;
+
+
+
+    @FXML
+    private void onActionStartNew() {
+        System.out.println("StartNew");
+
+    }
+
+    @FXML
+    private void onActionLoadGame() {
+        System.out.println("LoadGame");
+
+    }
+
+    @FXML
+    private void onActionOptions() {
+        System.out.println("Options");
+
+    }
+
+    @FXML
+    private void onActionQuit() {
+        System.out.println("Quit");
+
+    }
+}
