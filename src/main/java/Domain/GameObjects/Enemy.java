8388689b4d6dce52b2b77ab23cb0420196af811e
ataydaloglu <eataydaloglu@gmail.com>
2025-04-28 15:21:48 +0300

    updated enemy class

--- src/main/java/Domain/GameObjects/Enemy.java
+++ src/main/java/Domain/GameObjects/Enemy.java
@@ -1,4 +1,14 @@
 package Domain.GameObjects;
 
-public class Enemy {
+public class Enemy extends GameObject {
+
+    public Enemy(int xPos, int yPos, EnemyType enemyType) {
+        super(xPos, yPos);
+        this.enemyType = enemyType;
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
 }
