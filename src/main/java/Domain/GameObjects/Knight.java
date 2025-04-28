c564f96ca0cff2a735c02023edf5d6948054756b
ataydaloglu <eataydaloglu@gmail.com>
2025-04-28 16:03:20 +0300

    added subclasses for enemy

---
+++ src/main/java/Domain/GameObjects/Knight.java
@@ -0,0 +1,9 @@
+package Domain.GameObjects;
+
+public class Knight extends Enemy{
+
+    public Knight(int xPos, int yPos, String enemyType, int healthPoints) {
+        super(xPos, yPos, enemyType, healthPoints);
+    }
+
+}
