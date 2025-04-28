e5d21024c34468911f7b568f6586d95bb68c46be
ataydaloglu <eataydaloglu@gmail.com>
2025-04-28 16:06:49 +0300

    added attributes for enemy

--- src/main/java/Domain/GameObjects/Enemy.java
+++ src/main/java/Domain/GameObjects/Enemy.java
@@ -1,15 +1,16 @@
 package Domain.GameObjects;
 
 public class Enemy extends GameObject {
+
     String enemyType;
     int healthPoints;
-    
+    double speed;
 
-
-    public Enemy(int xPos, int yPos, String enemyType, int healthPoints) {
+    public Enemy(int xPos, int yPos, String enemyType, int healthPoints, double speed) {
         super(xPos, yPos);
         this.enemyType = enemyType;
         this.healthPoints = healthPoints;
+        this.speed = speed;
     }
 
     @Override
--- src/main/java/Domain/GameObjects/Goblin.java
+++ src/main/java/Domain/GameObjects/Goblin.java
@@ -2,7 +2,7 @@ package Domain.GameObjects;
 
 public class Goblin extends Enemy{
 
-    public Goblin(int xPos, int yPos, String enemyType, int healthPoints) {
-        super(xPos, yPos, enemyType, healthPoints);
+    public Goblin(int xPos, int yPos, String enemyType, int healthPoints, int speed) {
+        super(xPos, yPos, enemyType, healthPoints, speed);
     }
 }
