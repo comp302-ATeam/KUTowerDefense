cfa04f80753e80810937a8c2ed0220fcf30e8567
ataydaloglu <eataydaloglu@gmail.com>
2025-04-28 16:53:19 +0300

    Enemy class
    enemy class methods, subclasses goblin and knight, take damage method

--- src/main/java/Domain/GameObjects/Projectile.java
+++ src/main/java/Domain/GameObjects/Projectile.java
@@ -1,4 +1,11 @@
 package Domain.GameObjects;
 
 public class Projectile {
+    int damage;
+    String type;
+    public Projectile(int damage, String type) {
+        this.damage = damage;
+        this.type = type;
+
+    }
 }
