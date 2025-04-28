189b7378cefd203095aafe3e99f53b5d02c5ceea
ataydaloglu <eataydaloglu@gmail.com>
2025-04-28 16:53:19 +0300

    Enemy class
    enemy class methods, subclasses goblin and knight, take damage method

--- src/main/java/Domain/GameObjects/Knight.java
+++ src/main/java/Domain/GameObjects/Knight.java
@@ -1,9 +1,23 @@
 package Domain.GameObjects;
 
 public class Knight extends Enemy{
-
+    double speedMultiplier = 0.75;
     public Knight(int xPos, int yPos, String enemyType, int healthPoints, int speed) {
         super(xPos, yPos, enemyType, healthPoints, speed);
+        this.speed *= speedMultiplier;
     }
 
+    @Override
+    protected int CalcDamage(Projectile projectile) {
+        switch (projectile.type) {
+            case "Arrow":
+                return (int) (projectile.damage*0.75);
+            case "Magic":
+                return (int) (projectile.damage * 1.25);
+            case "Artillery":
+                return (int) (projectile.damage);
+            default:
+                return (int) (projectile.damage);
+        }
+    }
 }
--- src/main/java/Domain/GameObjects/Projectile.java
+++ src/main/java/Domain/GameObjects/Projectile.java
@@ -1,19 +1,11 @@
 package Domain.GameObjects;
 
-public class Projectile extends GameObject{
+public class Projectile {
     int damage;
     String type;
-    Enemy target;
-    public Projectile(int xPos, int yPos, int damage, String type, Enemy target) {
-        super(xPos, yPos);
+    public Projectile(int damage, String type) {
         this.damage = damage;
         this.type = type;
-        this.target = target;
-    }
-
-
-    @Override
-    public void update(double deltaTime) {
 
     }
 }
