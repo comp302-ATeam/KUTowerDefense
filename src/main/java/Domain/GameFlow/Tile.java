d55a563cc3c8683d515aa6ac1f0e66214e842f58
ataydaloglu <eataydaloglu@gmail.com>
2025-05-01 19:27:03 +0300

    Tile Class start

--- src/main/java/Domain/GameFlow/Tile.java
+++ src/main/java/Domain/GameFlow/Tile.java
@@ -1,4 +1,15 @@
 package Domain.GameFlow;
 
 public class Tile {
+    int row;
+    int col;
+
+    // World position (in pixels or JavaFX units)
+    double x;
+    double y;
+    // Whether this tile is already occupied by a Tower
+    boolean occupied = false;
+
+    // Size of the tile (width & height in pixels)
+    double size;
 }
