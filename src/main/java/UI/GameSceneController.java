b500a0338bc186d09d2e9262f8b9b6d53d625b92
rsahin22 <rsahin22@ku.edu.tr>
2025-05-01 14:16:14 +0300

    UI for Speed-Up, Resume, and Pause

---
+++ src/main/java/UI/GameSceneController.java
@@ -0,0 +1,31 @@
+package UI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+
+public class GameSceneController {
+    @FXML
+    private Button pauseButton;
+    @FXML
+    private Button resumeGame;
+    @FXML
+    private Button speedUp;
+
+    @FXML
+    private void handlePauseButton(ActionEvent event) {
+
+        System.out.println("Game Paused");
+    }
+    @FXML
+    private void handleResumeButton(ActionEvent event) {
+        System.out.println("Resumed Game");
+    }
+    @FXML
+    private void handleSpeedUpButton(ActionEvent event) {
+        System.out.println("Speed Up Game");
+    }
+}
+
+}
