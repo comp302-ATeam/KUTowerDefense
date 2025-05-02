fd76136ba011e0f6881e8e07a092d6ad12e3641d
ataydaloglu <eataydaloglu@gmail.com>
2025-05-02 21:41:44 +0300

    Return Button on Map Editor
    Icons, start new game scene is implemented. Return button for map editor is implemented.

---
+++ src/main/java/UI/InGameScene.java
@@ -0,0 +1,15 @@
+package UI;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.scene.Parent;
+public class InGameScene {
+
+}
