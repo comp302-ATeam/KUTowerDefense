fee7e4bce9a190f83588151208ccdd85e1b8f2e9
rsahin22 <rsahin22@ku.edu.tr>
2025-05-02 15:26:46 +0300

    Domain call in UI

--- src/main/java/UI/GameSceneController.java
+++ src/main/java/UI/GameSceneController.java
@@ -1,11 +1,14 @@
 package UI;
 
+import Domain.GameFlow.GameActionController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 
 
 public class GameSceneController {
+    private GameActionController gameActionController = new GameActionController();
+
     @FXML
     private Button pauseButton;
     @FXML
@@ -13,17 +16,20 @@ public class GameSceneController {
     @FXML
     private Button speedUp;
 
+
     @FXML
     private void handlePauseButton(ActionEvent event) {
-
+        gameActionController.pauseGame();
         System.out.println("Game Paused");
     }
     @FXML
     private void handleResumeButton(ActionEvent event) {
+        gameActionController.resumeGame();
         System.out.println("Resumed Game");
     }
     @FXML
     private void handleSpeedUpButton(ActionEvent event) {
+        gameActionController.speedUpGame();
         System.out.println("Speed Up Game");
     }
 }
