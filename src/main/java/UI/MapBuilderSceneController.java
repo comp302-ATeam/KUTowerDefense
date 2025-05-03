98e570dfeab02f231ce868f2ea63ecbd96992f3e
Cemkayim22 <ckayim22@ku.edu.tr>
2025-05-03 20:55:07 +0300

    map editor with load function

--- src/main/java/UI/MapBuilderSceneController.java
+++ src/main/java/UI/MapBuilderSceneController.java
@@ -2,7 +2,11 @@ package UI;
 
 import Domain.GameFlow.MapLoader;
 import Domain.GameFlow.TileSetLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,7 +15,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class MapBuilderSceneController {
 
@@ -40,8 +44,14 @@ public class MapBuilderSceneController {
     }
 
     @FXML
-    public void onReturn() {
-        showTemporaryWindow();
+    public void onReturn(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/StartScene.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+        //stage.setMaximized(true);
+
     }
 
 
