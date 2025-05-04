0451c5e49bfd57920815f9337a564b8c0b88ee6e
Cemkayim22 <ckayim22@ku.edu.tr>
2025-05-04 19:16:02 +0300

    adjusted path returns

--- src/main/java/Domain/GameFlow/TileMap.java
+++ src/main/java/Domain/GameFlow/TileMap.java
@@ -1,5 +1,8 @@
 package Domain.GameFlow;
 
+import javafx.geometry.Bounds;
+import javafx.scene.layout.GridPane;
+
 import java.io.Serializable;
 
 public class TileMap implements Serializable {
@@ -19,22 +22,33 @@ public class TileMap implements Serializable {
         this.tileGrid = tileGrid;
         this.path = path;
 
-        for (Tile tile : path) {
-            System.out.println(tile.position);
-        }
+//        for (Tile tile : path) {
+//            System.out.println(tile.position);
+//        }
 
     }
 
     public Tile[][] getTileGrid() {
         return tileGrid;
     }
-    public Tile[] getPath() {
-        return path;
-    }
 
-    public void setPath(Tile[] path) {
-        this.path = path;
 
+
+    public Vector2<Double>[] getPath(GridPane gridPane , int tileSize){
+
+        Bounds screenBounds = gridPane.localToScreen(gridPane.getBoundsInLocal());
+
+        Vector2<Double> factor = new Vector2<>(screenBounds.getMinX() + (tileSize /2) ,screenBounds.getMinY() + (tileSize /2));
+
+        Vector2<Double>[] new_path = new Vector2[this.path.length];
+
+
+
+        for (int i = 0; i < this.path.length; i++) {
+            Vector2<Integer> relativePos = path[i].position;
+            new_path[i] = new Vector2<>(factor.x + relativePos.x * tileSize ,factor.y +  relativePos.y * tileSize);
+        }
+        return new_path;
     }
 
     public Vector2<Integer> getSize() {
