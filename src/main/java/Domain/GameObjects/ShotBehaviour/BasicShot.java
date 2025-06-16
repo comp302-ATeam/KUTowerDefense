506d5bd199dd820d78a6092f023355f9b9583081
ataydaloglu <eataydaloglu@gmail.com>
2025-06-16 22:47:56 +0300

    fireball animation done

--- src/main/java/Domain/GameObjects/ShotBehaviour/BasicShot.java
+++ src/main/java/Domain/GameObjects/ShotBehaviour/BasicShot.java
@@ -1,10 +1,15 @@
 package Domain.GameObjects.ShotBehaviour;
 
+import Domain.GameFlow.AnimationPlayer;
 import Domain.GameObjects.Enemy;
 import Domain.GameObjects.Projectile;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 
 public class BasicShot extends BaseBehaviour {
-
+    private static final int EXPLOSION_FRAMES = 7;
+    private static final int EXPLOSION_COLS = 7;
+    private static final double EXPLOSION_FPS = 12.0;
 
     public BasicShot(Projectile projectile) {
         super(projectile);
@@ -15,6 +20,23 @@ public class BasicShot extends BaseBehaviour {
     @Override
     public boolean Shoot(Enemy target) {
         target.takeDamage(projectile);
+        if(projectile.getType()=="MagicSpell"){
+            Pane gamePane = (Pane) projectile.getParent();
+            Image explosionSheet = new Image(getClass().getResourceAsStream("/Assets/Effects/Fire.png"));
+            AnimationPlayer explosionAnim = new AnimationPlayer(explosionSheet, EXPLOSION_FRAMES, EXPLOSION_COLS, EXPLOSION_FPS);
+            explosionAnim.setLooping(false);
+            double explosionX = target.getX() + target.frameWidth / 2;
+            double explosionY = target.getY() + target.frameHeight / 2;
+
+            explosionAnim.setOnComplete(() -> {
+                if (explosionAnim.isPlaying()) {
+                    explosionAnim.stop();
+                }
+            });
+
+            // Play the explosion animation
+            explosionAnim.playAt(explosionX, explosionY, gamePane);
+        }
         return false;
     }
 }
